03a3cdc6ef64ced2c14f315f60ab3358507e801d
Kos <turchakkonstantin@gmail.com>
2023-05-27 19:26:59 +0300

    Added model

---
+++ BoockOfRecipes/Model/Recipes.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoockOfRecipes.Model
+{
+    class Recipes
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Country { get; set; }
+        public string? RecipeType { get; set; }
+        public string? ManualName { get; set; }
+        public List<string>? Manual { get; set; }
+    }
+}
